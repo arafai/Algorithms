7961082b0fe1e60672359047df6afd192c377426
Andrei Rafai <rafai@eloquentix.com>
2015-06-20 23:10:13 +0300

    initial commit

--- src/codility/MinAvgTwoSlice.scala
+++ src/codility/MinAvgTwoSlice.scala
@@ -1,31 +1,17 @@
 package codility
 
-import scala.util.control.Breaks._
-
 object MinAvgTwoSlice extends App {
 
   def solution(array: Array[Int]): Int = {
-    val n = array.length
-    val map = scala.collection.mutable.Map[Int, Boolean]()
-    var steps = n
-    var index = 0
-    breakable {
-      array.iterator.foreach { element =>
-        if (element <= n && map.get(element).isEmpty) {
-          map.put(element, true)
-          steps -= 1
-        } else {
-          break
-        }
-        index += 1
-      }
-    }
-
-    if (steps == 0 && index == n)
-      1
-    else
-      0
+    array.sliding(2).zipWithIndex
+      .foldLeft((Integer.MAX_VALUE, 0)) { case ((minAvg, indexMin), (Array(start, end), index)) =>
+      val avg = (start + end) / 2
+      if (avg < minAvg)
+        (avg, index)
+      else
+        (minAvg, indexMin)
+    }._2
   }
 
-  println(solution(Array(4, 1, 3, 2)))
+  println(solution(Array(4, 2, 2, 5, 1, 5, 8)))
 }
