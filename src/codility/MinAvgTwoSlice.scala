cc3555cdccd243de61be0360fe9b156dc695660c
Andrei Rafai <rafai@eloquentix.com>
2015-06-20 23:46:02 +0300

    initial commit

--- src/codility/MinAvgTwoSlice.scala
+++ src/codility/MinAvgTwoSlice.scala
@@ -3,9 +3,11 @@ package codility
 object MinAvgTwoSlice extends App {
 
   def solution(array: Array[Int]): Int = {
-    array.sliding(2).zipWithIndex
-      .foldLeft((Integer.MAX_VALUE, 0)) { case ((minAvg, indexMin), (Array(start, end), index)) =>
-      val avg = (start + end) / 2
+    // a lot of objects are created - performance wise
+    // looks concise
+    array.sliding(2).zipWithIndex.++(array.sliding(3).zipWithIndex)
+      .foldLeft((Double.MaxValue, 0)) { case ((minAvg, indexMin), (list, index)) =>
+      val avg = list.sum.toDouble / list.size
       if (avg < minAvg)
         (avg, index)
       else
@@ -14,4 +16,5 @@ object MinAvgTwoSlice extends App {
   }
 
   println(solution(Array(4, 2, 2, 5, 1, 5, 8)))
+  println(solution(Array(1, 6, 3, 15, 11, 15, 18)))
 }
